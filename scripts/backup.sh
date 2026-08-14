7ce736924aad41edb414ac9c41e8b123b3924123
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-08-14 19:42:46 +0100

    feat: config load and basic checks before backup starts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -32,9 +32,35 @@ check_config() {
     fi
 }
 
+load_config() {
+    check_config
+
+    set -a
+    # shellcheck source=/dev/null
+    source "$CONFIG_FILE"
+    set +a
+}
+
+load_config
 check_config DLT_BACKUP_BORG_REPO
 
-if [ -d "$DLT_BACKUP_BORG_REPO" ]; then
+DLT_BACKUP_BORG_REPO="$(readlink -f "$DLT_BACKUP_BORG_REPO")"
+
+if [ ! -d "$DLT_BACKUP_BORG_REPO" ]; then
     log ERROR "Borg repository not a directory" "$DLT_BACKUP_BORG_REPO"
     exit 1
 fi
+
+if ! borg info "$DLT_BACKUP_BORG_REPO" &>/dev/null; then
+    if [ -n "$(find "$DLT_BACKUP_BORG_REPO" -mindepth 1 -maxdepth 1)" ]; then
+        log ERROR "Borg repository directory not empty" "$DLT_BACKUP_BORG_REPO"
+        exit 1
+    fi
+
+    log INFO "Initializing borg repository" "$DLT_BACKUP_BORG_REPO"
+    # TODO borg init
+fi
+
+# TODO borg create (backup)
+# TODO borg prune
+# TODO borg compact
